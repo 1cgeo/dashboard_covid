e8d0e7c664a8c3fe19dd1f8b8f90b59cccba1a09
Felipe Diniz <diniz.ime@gmail.com>
2020-05-24 11:17:59 -0300

    read csv once on starttup

--- server/models/maptheme.js
+++ server/models/maptheme.js
@@ -16,6 +16,19 @@ const SUMMARY_CITIES_FILE_PATH = path.join(
   "cidades.csv"
 );
 
+let dadosEstados;
+csv()
+  .fromFile(SUMMARY_STATES_FILE_PATH)
+  .then(function (jsonData) {
+    dadosEstados = jsonData;
+  });
+let dadosCidades;
+csv()
+  .fromFile(SUMMARY_CITIES_FILE_PATH)
+  .then(function (jsonData) {
+    dadosCidades = jsonData;
+  });
+
 getGeoJsonCollectionTemplate = () => {
   return {
     type: "FeatureCollection",
@@ -35,84 +48,64 @@ getFeaturePointTemplate = () => {
 };
 
 module.exports.getChoroplethStates = (cb) => {
-  csv()
-    .fromFile(SUMMARY_STATES_FILE_PATH)
-    .then(function (jsonData) {
-      var choroplethStatesData = jsonData.slice(1).map((info) => {
-        return {
-          nrDiasDobraCasos: info.nrDiasDobraCasos,
-          nrDiasDobraMortes: info.nrDiasDobraMortes,
-          CD_GEOCUF: info.CD_GEOCUF,
-        };
-      });
-      cb(choroplethStatesData);
-    });
+  var choroplethStatesData = dadosEstados.slice(1).map((info) => {
+    return {
+      nrDiasDobraCasos: info.nrDiasDobraCasos,
+      nrDiasDobraMortes: info.nrDiasDobraMortes,
+      CD_GEOCUF: info.CD_GEOCUF,
+    };
+  });
+  cb(choroplethStatesData);
 };
 
 module.exports.getCircleStates = (cb) => {
-  csv()
-    .fromFile(SUMMARY_STATES_FILE_PATH)
-    .then(function (jsonData) {
-      let geojson = getGeoJsonCollectionTemplate();
-      geojson.features = jsonData.slice(1).map((info) => {
-        let feat = getFeaturePointTemplate();
-        feat.geometry.coordinates = [info.CENTROID_X, info.CENTROID_Y];
-        feat.properties.totalCases = info.totalCases;
-        feat.properties.deaths = info.deaths;
-        feat.properties.state = info.state;
-        feat.properties.city = info.city;
-        return feat;
-      });
-      cb(geojson);
-    });
+  let geojson = getGeoJsonCollectionTemplate();
+  geojson.features = dadosEstados.slice(1).map((info) => {
+    let feat = getFeaturePointTemplate();
+    feat.geometry.coordinates = [info.CENTROID_X, info.CENTROID_Y];
+    feat.properties.totalCases = info.totalCases;
+    feat.properties.deaths = info.deaths;
+    feat.properties.state = info.state;
+    feat.properties.city = info.city;
+    return feat;
+  });
+  cb(geojson);
 };
 
 module.exports.getChoroplethCities = (cb) => {
-  csv()
-    .fromFile(SUMMARY_CITIES_FILE_PATH)
-    .then(function (jsonData) {
-      var choroplethStatesData = jsonData.slice(1).map((info) => {
-        return {
-          nrDiasDobraCasos: info.nrDiasDobraCasos,
-          nrDiasDobraMortes: info.nrDiasDobraMortes,
-          CD_GEOCMU: info.ibgeID,
-        };
-      });
-      cb(choroplethStatesData);
-    });
+  var choroplethStatesData = dadosCidades.slice(1).map((info) => {
+    return {
+      nrDiasDobraCasos: info.nrDiasDobraCasos,
+      nrDiasDobraMortes: info.nrDiasDobraMortes,
+      CD_GEOCMU: info.ibgeID,
+    };
+  });
+  cb(choroplethStatesData);
 };
 
 module.exports.getHeatCities = (cb) => {
-  csv()
-    .fromFile(SUMMARY_CITIES_FILE_PATH)
-    .then(function (infoDeatilsObj) {
-      var heatCitiesData = infoDeatilsObj.map((info) => {
-        info.latlong = [info.lat, info.lon];
-        return {
-          latlong: info.latlong,
-          deaths: info.deaths,
-          totalCases: info.totalCases,
-        };
-      });
-      cb(heatCitiesData);
-    });
+  var heatCitiesData = dadosCidades.map((info) => {
+    info.latlong = [info.lat, info.lon];
+    return {
+      latlong: info.latlong,
+      deaths: info.deaths,
+      totalCases: info.totalCases,
+    };
+  });
+  cb(heatCitiesData);
 };
 
 module.exports.getCircleCities = (cb) => {
-  csv()
-    .fromFile(SUMMARY_CITIES_FILE_PATH)
-    .then(function (infoDeatilsObj) {
-      let geojson = getGeoJsonCollectionTemplate();
-      geojson.features = infoDeatilsObj.map((info) => {
-        info.lnglat = [info.lon, info.lat];
-        let feat = getFeaturePointTemplate();
-        feat.geometry.coordinates = info.lnglat;
-        feat.properties.totalCases = info.totalCases;
-        feat.properties.deaths = info.deaths;
-        feat.properties.state = info.state;
-        feat.properties.city = info.city;
-        return feat;
-      });
-      cb(geojson);
-    });
+  let geojson = getGeoJsonCollectionTemplate();
+  geojson.features = dadosCidades.map((info) => {
+    info.lnglat = [info.lon, info.lat];
+    let feat = getFeaturePointTemplate();
+    feat.geometry.coordinates = info.lnglat;
+    feat.properties.totalCases = info.totalCases;
+    feat.properties.deaths = info.deaths;
+    feat.properties.state = info.state;
+    feat.properties.city = info.city;
+    return feat;
+  });
+  cb(geojson);
 };
