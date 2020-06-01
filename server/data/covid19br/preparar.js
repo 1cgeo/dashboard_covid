b30df2b37d741f63e015d86c45ef9a6cbdeb16ad
Felipe Diniz <diniz.ime@gmail.com>
2020-05-31 21:45:21 -0300

    preparo

---
+++ server/data/covid19br/preparar.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const { modify_csv_estado, csv_brasil, modify_csv_cidade } = require("./util");
+
+modify_csv_estado(
+  path.join(__dirname, "estados_original.csv"),
+  path.join(__dirname, "estados.csv")
+);
+csv_brasil(
+  path.join(__dirname, "estados_original.csv"),
+  path.join(__dirname, "brasil.csv")
+);
+
+modify_csv_cidade(
+  path.join(__dirname, "cidades_original.csv"),
+  path.join(__dirname, "coords_cidade_original.csv"),
+  path.join(__dirname, "cidades.csv")
+);
