a2e8ee2062901162908d18bf021d1ac14540a4ec
Felipe Diniz <diniz.ime@gmail.com>
2020-06-29 19:16:08 -0300

    cores taxa de crescimento

--- public/js/choroplethRateTheme.js
+++ public/js/choroplethRateTheme.js
@@ -1,254 +1,270 @@
 class ChoroplethRateLayer extends Layer {
-    constructor(newOptions) {
-        super(newOptions)
-        this.currentProcessKey = ""
-        this.vectorTiles = []
-        this.limits = []
-        this.scenes = []
-        this.rangeData = null
-        this.create()
+  constructor(newOptions) {
+    super(newOptions);
+    this.currentProcessKey = "";
+    this.vectorTiles = [];
+    this.limits = [];
+    this.scenes = [];
+    this.rangeData = null;
+    this.create();
+  }
+
+  startAnimation() {
+    this.updateAnimation();
+  }
+
+  stopAnimation() {
+    this.updateAnimation();
+  }
+
+  updateAnimation() {
+    var processKey = this.createUUID();
+    this.currentProcessKey = processKey;
+    var jsonData = this.getJsonData();
+    if (jsonData.length < 1) {
+      return;
     }
-
-    startAnimation() {
-        this.updateAnimation()
+    var mapLayers = this.options.map.getCurrentLayerOptions().mapLayers;
+    if (mapLayers.length < 1) {
+      return;
     }
+    var mainLayer = mapLayers.find((l) => l.main);
+    this.lastData = jsonData;
 
-    stopAnimation() {
-        this.updateAnimation()
+    if (processKey !== this.currentProcessKey) return;
+
+    this.currentPane = this.currentPane === "fill1" ? "fill2" : "fill1";
+    if (this.currentPane == "fill1") {
+      this.options.map.getMap().getPane("fill2").style.zIndex = 2045;
+      this.options.map.getMap().getPane("fill1").style.zIndex = 2030;
     }
+    var layer = this.createVectorGrid(
+      mainLayer,
+      this.options.attributeName,
+      this.options.attributeNameSecondary,
+      false
+    );
+    this.options.map.getFeatureGroup().addLayer(layer);
+    this.mainVectorTile = layer;
+    this.scenes.push(layer);
+    setTimeout(() => {
+      if (this.scenes.length > 1) {
+        var l = this.scenes.shift();
+        this.options.map.getFeatureGroup().removeLayer(l);
+      }
+    }, 1500);
+  }
 
-    updateAnimation() {
-        var processKey = this.createUUID()
-        this.currentProcessKey = processKey
-        var jsonData = this.getJsonData()
-        if (jsonData.length < 1) { return }
-        var mapLayers = this.options.map.getCurrentLayerOptions().mapLayers
-        if (mapLayers.length < 1) { return }
-        var mainLayer = mapLayers.find((l) => l.main)
-        this.lastData = jsonData
-
-        if (processKey !== this.currentProcessKey) return
-
-        this.currentPane = (this.currentPane === 'fill1') ? 'fill2' : 'fill1'
-        if (this.currentPane == 'fill1') {
-            this.options.map.getMap().getPane('fill2').style.zIndex = 2045
-            this.options.map.getMap().getPane('fill1').style.zIndex = 2030
-        }
-        var layer = this.createVectorGrid(
-            mainLayer,
-            this.options.attributeName,
-            this.options.attributeNameSecondary,
-            false,
-        )
-        this.options.map.getFeatureGroup().addLayer(layer)
-        this.mainVectorTile = layer
-        this.scenes.push(layer)
-        setTimeout(() => {
-                if (this.scenes.length > 1) {
-                    var l = this.scenes.shift()
-                    this.options.map.getFeatureGroup().removeLayer(l)
-                }
-            }, 1500)
+  remove() {
+    this.currentProcessKey = "";
+    if (this.currentLegend) {
+      this.options.map.getMap().removeControl(this.currentLegend);
+      this.currentLegend = null;
     }
-
-    remove() {
-        this.currentProcessKey = ""
-        if (this.currentLegend) {
-            this.options.map.getMap().removeControl(this.currentLegend)
-            this.currentLegend = null
-        }
-        for (var i = this.vectorTiles.length; i--;) {
-            this.options.map.getFeatureGroup().removeLayer(this.vectorTiles[i])
-        }
-        this.layers = []
-        for (var i = this.limits.length; i--;) {
-            this.options.map.getFeatureGroup().removeLayer(this.limits[i])
-        }
-        this.limits = []
-        for (var i = this.scenes.length; i--;) {
-            this.options.map.getFeatureGroup().removeLayer(this.scenes[i])
-        }
-        this.scenes = []
-        this.rangeData = null
+    for (var i = this.vectorTiles.length; i--; ) {
+      this.options.map.getFeatureGroup().removeLayer(this.vectorTiles[i]);
     }
-
-    loadPanels() {
-        if (!this.options.map.getMap().getPane('limitpane')) {
-            this.options.map.getMap().createPane('limitpane')
-            this.options.map.getMap().getPane('limitpane').style.zIndex = 2050
-        }
-        if (!this.options.map.getMap().getPane('fill1')) {
-            this.options.map.getMap().createPane('fill1')
-            this.options.map.getMap().getPane('fill1').style.zIndex = 2045
-        }
-        if (!this.options.map.getMap().getPane('fill2')) {
-            this.options.map.getMap().createPane('fill2')
-            this.options.map.getMap().getPane('fill2').style.zIndex = 2030
-        }
+    this.layers = [];
+    for (var i = this.limits.length; i--; ) {
+      this.options.map.getFeatureGroup().removeLayer(this.limits[i]);
     }
-
-    getJsonData() {
-        if (this.options.layerId == 0) {
-            return this.options.map.getDataSource().getStateChoroplethData()
-        }
-        return this.options.map.getDataSource().getCityChoroplethData()
+    this.limits = [];
+    for (var i = this.scenes.length; i--; ) {
+      this.options.map.getFeatureGroup().removeLayer(this.scenes[i]);
     }
+    this.scenes = [];
+    this.rangeData = null;
+  }
 
-    create() {
-        var processKey = this.createUUID()
-        this.currentProcessKey = processKey
-        var jsonData = this.getJsonData()
-        if (jsonData.length < 1) { return }
-        var mapLayers = this.options.map.getCurrentLayerOptions().mapLayers
-        this.lastData = jsonData
-        if (mapLayers.length < 1) { return }
-        if (processKey !== this.currentProcessKey) return
-        this.loadPanels()
-        this.loadLimits(mapLayers)
-        for (var i = mapLayers.length; i--;) {
-            this.idField = mapLayers[i].idField
-            var loadOtherStyle = (mapLayers[i].main) ? false : true
-            this.currentPane = 'fill1'
-            var layer = this.createVectorGrid(
-                mapLayers[i],
-                this.options.attributeName,
-                this.options.attributeNameSecondary,
-                loadOtherStyle,
-            )
-            this.options.map.getFeatureGroup().addLayer(layer)
-            this.vectorTiles.push(layer)
-            if (mapLayers[i].main) {
-                this.mainVectorTile = layer
-                this.scenes.push(layer)
-            }
-        }
-        if (!this.currentLegend) this.createLegend()
+  loadPanels() {
+    if (!this.options.map.getMap().getPane("limitpane")) {
+      this.options.map.getMap().createPane("limitpane");
+      this.options.map.getMap().getPane("limitpane").style.zIndex = 2050;
     }
-
-    loadLimits(mapLayers) {
-        for (var i = mapLayers.length; i--;) {
-            var idField = mapLayers[i].idField
-            var layer = L.vectorGrid.protobuf(
-                mapLayers[i].url, {
-                    pane: 'limitpane',
-                    rendererFactory: L.canvas.tile,
-                    vectorTileLayerStyles: {
-                        "data": {
-                            weight: (this.options.layerId == 0) ? 0.5 : (mapLayers[i].main) ? 0.1 : 0.5,
-                            opacity: 1,
-                            color: 'black'
-                        }
-                    },
-                    interactive: true,
-                    getFeatureId: (feature) => {
-                        return feature.properties[idField]
-                    }
-                }
-            )
-            this.options.map.getFeatureGroup().addLayer(layer)
-            this.limits.push(layer)
-        }
+    if (!this.options.map.getMap().getPane("fill1")) {
+      this.options.map.getMap().createPane("fill1");
+      this.options.map.getMap().getPane("fill1").style.zIndex = 2045;
     }
-
-    createVectorGrid(mapLayer, attrLabel1, attrLabel2, loadDefaultStyle) {
-        var layer = L.vectorGrid.protobuf(
-            mapLayer.url, {
-                rendererFactory: L.canvas.tile,
-                pane: this.currentPane,
-                vectorTileLayerStyles: {
-                    "data": (feat) => {
-                        if (loadDefaultStyle) return mapLayer.style
-                        var idx = this.lastData.ids.indexOf(feat[mapLayer.idField])
-                        if (idx >= 0) {
-                            feat.rate = +this.lastData.data[idx][attrLabel1]
-                            feat[attrLabel2] = +this.lastData.data[idx][attrLabel2]
-                            return this.getStyle(feat.rate, feat[attrLabel2])
-                        }
-                        feat.rate = 0
-                        feat[attrLabel2] = 0
-                        return this.getStyle(feat.rate, feat[attrLabel2])
-                    }
-                },
-                interactive: true,
-                getFeatureId: (feature) => {
-                    return feature.properties[mapLayer.idField]
-                }
-            }
-        )
-        return layer
+    if (!this.options.map.getMap().getPane("fill2")) {
+      this.options.map.getMap().createPane("fill2");
+      this.options.map.getMap().getPane("fill2").style.zIndex = 2030;
     }
+  }
 
-    showPopup(e) {
-        L.popup({
-                pane: 'popup',
-                closeButton: false
-            })
-            .setLatLng(
-                this.options.map.getMap().layerPointToLatLng(e.layerPoint)
-            )
-            .setContent(this.getPopupContent(e))
-            .openOn(this.options.map.getMap());
+  getJsonData() {
+    if (this.options.layerId == 0) {
+      return this.options.map.getDataSource().getStateChoroplethData();
     }
+    return this.options.map.getDataSource().getCityChoroplethData();
+  }
 
-    getStyle(attrLabel1, attrLabel2) {
-        return {
-            weight: 0,
-            opacity: 0,
-            color: 'white',
-            dashArray: '2',
-            fill: true,
-            fillOpacity: 1,
-            fillColor: this.getColor(attrLabel1, attrLabel2)
-        };
+  create() {
+    var processKey = this.createUUID();
+    this.currentProcessKey = processKey;
+    var jsonData = this.getJsonData();
+    if (jsonData.length < 1) {
+      return;
     }
-
-    getColor(attrLabel1, attrLabel2) {
-        var colors = this.getHexColors()
-        if (+attrLabel2 === 0) {
-            return '#eeeeee'
-        } else if (+attrLabel2 < this.getLimiteValue()) {
-            return '#bdbdbd'
-        } else {
-            return attrLabel1 < 7 ? colors[3] :
-                attrLabel1 < 14 ? colors[2] :
-                attrLabel1 < 30 ? colors[1] :
-                colors[0];
-        }
+    var mapLayers = this.options.map.getCurrentLayerOptions().mapLayers;
+    this.lastData = jsonData;
+    if (mapLayers.length < 1) {
+      return;
     }
-
-    getAttributeName() {
-        return this.options.attributeNameSecondary
+    if (processKey !== this.currentProcessKey) return;
+    this.loadPanels();
+    this.loadLimits(mapLayers);
+    for (var i = mapLayers.length; i--; ) {
+      this.idField = mapLayers[i].idField;
+      var loadOtherStyle = mapLayers[i].main ? false : true;
+      this.currentPane = "fill1";
+      var layer = this.createVectorGrid(
+        mapLayers[i],
+        this.options.attributeName,
+        this.options.attributeNameSecondary,
+        loadOtherStyle
+      );
+      this.options.map.getFeatureGroup().addLayer(layer);
+      this.vectorTiles.push(layer);
+      if (mapLayers[i].main) {
+        this.mainVectorTile = layer;
+        this.scenes.push(layer);
+      }
     }
+    if (!this.currentLegend) this.createLegend();
+  }
 
-    getLimiteValue() {
-        return (this.getAttributeName() === 'deaths') ? 10 : 100
+  loadLimits(mapLayers) {
+    for (var i = mapLayers.length; i--; ) {
+      var idField = mapLayers[i].idField;
+      var layer = L.vectorGrid.protobuf(mapLayers[i].url, {
+        pane: "limitpane",
+        rendererFactory: L.canvas.tile,
+        vectorTileLayerStyles: {
+          data: {
+            weight:
+              this.options.layerId == 0 ? 0.5 : mapLayers[i].main ? 0.1 : 0.5,
+            opacity: 1,
+            color: "black",
+          },
+        },
+        interactive: true,
+        getFeatureId: (feature) => {
+          return feature.properties[idField];
+        },
+      });
+      this.options.map.getFeatureGroup().addLayer(layer);
+      this.limits.push(layer);
     }
+  }
 
-    getHexColors() {
-        return (this.getAttributeName() === 'deaths') ? ['#fee5d9', '#fcae91', '#fb6a4a', '#cb181d'] : ['#ffffcc', '#a1dab4', '#41b6c4', '#225ea8']
+  createVectorGrid(mapLayer, attrLabel1, attrLabel2, loadDefaultStyle) {
+    var layer = L.vectorGrid.protobuf(mapLayer.url, {
+      rendererFactory: L.canvas.tile,
+      pane: this.currentPane,
+      vectorTileLayerStyles: {
+        data: (feat) => {
+          if (loadDefaultStyle) return mapLayer.style;
+          var idx = this.lastData.ids.indexOf(feat[mapLayer.idField]);
+          if (idx >= 0) {
+            feat.rate = +this.lastData.data[idx][attrLabel1];
+            feat[attrLabel2] = +this.lastData.data[idx][attrLabel2];
+            return this.getStyle(feat.rate, feat[attrLabel2]);
+          }
+          feat.rate = 0;
+          feat[attrLabel2] = 0;
+          return this.getStyle(feat.rate, feat[attrLabel2]);
+        },
+      },
+      interactive: true,
+      getFeatureId: (feature) => {
+        return feature.properties[mapLayer.idField];
+      },
+    });
+    return layer;
+  }
+
+  showPopup(e) {
+    L.popup({
+      pane: "popup",
+      closeButton: false,
+    })
+      .setLatLng(this.options.map.getMap().layerPointToLatLng(e.layerPoint))
+      .setContent(this.getPopupContent(e))
+      .openOn(this.options.map.getMap());
+  }
+
+  getStyle(attrLabel1, attrLabel2) {
+    return {
+      weight: 0,
+      opacity: 0,
+      color: "white",
+      dashArray: "2",
+      fill: true,
+      fillOpacity: 1,
+      fillColor: this.getColor(attrLabel1, attrLabel2),
+    };
+  }
+
+  getColor(attrLabel1, attrLabel2) {
+    var colors = this.getHexColors();
+    if (+attrLabel2 === 0) {
+      return "#eeeeee";
+    } else if (+attrLabel2 < this.getLimiteValue()) {
+      return "#bdbdbd";
+    } else {
+      return attrLabel1 < 7
+        ? colors[3]
+        : attrLabel1 < 14
+        ? colors[2]
+        : attrLabel1 < 30
+        ? colors[1]
+        : colors[0];
     }
+  }
 
-    getLegendContent() {
-        var colors = this.getHexColors()
-        var tag = (this.getAttributeName() === 'deaths') ? 'Óbitos' : 'Casos'
-        return `<div class="grid-choropleth-legend">
+  getAttributeName() {
+    return this.options.attributeNameSecondary;
+  }
+
+  getLimiteValue() {
+    return this.getAttributeName() === "deaths" ? 10 : 100;
+  }
+
+  getHexColors() {
+    return this.getAttributeName() === "deaths"
+      ? ["#f2f0f7", "#cbc9e2", "#9e9ac8", "#6a51a3"]
+      : ["#fee5d9", "#fcae91", "#fb6a4a", "#cb181d"];
+  }
+
+  getLegendContent() {
+    var colors = this.getHexColors();
+    var tag = this.getAttributeName() === "deaths" ? "Óbitos" : "Casos";
+    return `<div class="grid-choropleth-legend">
                 <div class="y">
                     <div>${tag} atualmente dobrando a cada...</div>
                 </div>
                 <div class="a" style="width:60px; height:15px; background-color: none; border-right:solid">
-                    <div style="width:60px; height:10px; background-color: ${colors[3]};">
+                    <div style="width:60px; height:10px; background-color: ${
+                      colors[3]
+                    };">
                     </div>
                 </div>
                 <div class="b" style="width:60px; height:15px; background-color: none; border-right:solid">
-                    <div style="width:60px; height:10px; background-color: ${colors[2]};">
+                    <div style="width:60px; height:10px; background-color: ${
+                      colors[2]
+                    };">
                     </div>
                 </div>
                 <div class="c" style="width:60px; height:15px; background-color: none; border-right:solid">
-                    <div style="width:60px; height:10px; background-color: ${colors[1]};">
+                    <div style="width:60px; height:10px; background-color: ${
+                      colors[1]
+                    };">
                     </div>
                 </div>
                 <div class="d" style="width:80px; height:15px; background-color: none;">
-                    <div style="width:60px; height:10px; background-color: ${colors[0]};">
+                    <div style="width:60px; height:10px; background-color: ${
+                      colors[0]
+                    };">
                     </div>
                 </div>
                 <div class="e" style="width:70px; height:15px; background-color: none;">
@@ -274,17 +290,17 @@ class ChoroplethRateLayer extends Layer {
                 <div class="m" style="width:80px; height:15px; background-color: none;">
                     <div>Não reportados</div>
                 </div>
-            </div>`
-    }
+            </div>`;
+  }
 
-    createLegend() {
-        var legend = L.control({ position: 'bottomright' });
-        legend.onAdd = (map) => {
-            var div = L.DomUtil.create('div', '')
-            div.innerHTML = this.getLegendContent()
-            return div;
-        }
-        legend.addTo(this.options.map.getMap());
-        this.currentLegend = legend
-    }
+  createLegend() {
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = (map) => {
+      var div = L.DomUtil.create("div", "");
+      div.innerHTML = this.getLegendContent();
+      return div;
+    };
+    legend.addTo(this.options.map.getMap());
+    this.currentLegend = legend;
+  }
 }
